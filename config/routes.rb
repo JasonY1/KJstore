b78b3fc4445044e45687f4a9afb9e814a8157544
Korean Jesus <jasonh.yi@gmail.com>
2019-06-29 09:41:26 +0000

    Updated routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
+  root to: 'pages#Home'
+  get 'pages/Program'
+  get 'pages/About'
+  get 'pages/Media'
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: '/shop'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
